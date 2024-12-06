7892707c99d10f2979444a49cab50947d67ceafc
Rishika Jaiswal <jaisrishika@gmail.com>
2024-12-06 23:32:42 +0530

    second

--- src/App.js
+++ src/App.js
@@ -2,24 +2,20 @@ import React from 'react';
 import articles from './Api/data';
 import Card from './Componennt/Card';
 export default function App() {
+  const[isMobile,setIsMobile]= React.useState(false)
   const [currentIndex, setCurrentIndex] = React.useState(0);
 
   let touchStartX = 0;
   let touchEndX = 0;
 
   const handleTouchStart = (e) => {
-    if (e.touches.length === 2) {
       console.log("hello1")
       touchStartX = e.touches[0].clientX; // Get the X position of the first finger
-    }
   };
 
   const handleTouchMove = (e) => {
-    if (e.touches.length === 2){
       console.log("hello2")
       touchEndX = e.touches[0].clientX;
-    }
-
   };
 
   const handleTouchEnd = () => {
@@ -38,29 +34,72 @@ export default function App() {
       }
     }
   };
+  React.useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const leftHandle = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
+  const rightHandle = () => {
+    if (currentIndex < articles.length - 1) {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
+
   return (
     <div 
-      className='bg-black flex flex-row py-4 px-4 md:flex-row w-screen h-screen overflow-x-hidden px-50 '
+      className='bg-black items-center flex flex-row flex-wrap py-4 px-4 h-screen overflow-x-hidden justify-center'
       onTouchStart={handleTouchStart}
       onTouchMove={handleTouchMove}
       onTouchEnd={handleTouchEnd}
     >
-        {/* {articles.map((item,idx) => {
-          return ( */}
+      <button className="leftBtn text-white mr-10" onClick={leftHandle} > prev </button>
+      
+    {
+      isMobile===false?(
+        articles.slice(currentIndex,currentIndex+3).map((item,idx) => {
+          return (
+          <div className='inline-block min-w-max h-128'>
             <Card 
-              key={currentIndex} 
-              title={articles[currentIndex].title} 
-              date={articles[currentIndex].publishedAt.slice(0,10)} 
+              key={idx}  
+              title={item.title} 
+              date={item.publishedAt.slice(0,10)} 
               time={articles[currentIndex].publishedAt.slice(12,16)}
-              url={articles[currentIndex].url} 
-              image={articles[currentIndex].urlToImage} 
-              description={articles[currentIndex].description}
-              content={articles[currentIndex].content}
+              url={item.url} 
+              image={item.urlToImage} 
+              content={item.content}
               source={articles[currentIndex].source.name}
               author={articles[currentIndex].author}
-            />
-        {/* )})} */}
+
+              />
+          </div>
+        )})
+        ):(
+          <Card key={currentIndex} 
+               title={articles[currentIndex].title} 
+               date={articles[currentIndex].publishedAt.slice(0,10)} 
+               time={articles[currentIndex].publishedAt.slice(12,16)}
+               url={articles[currentIndex].url} 
+               image={articles[currentIndex].urlToImage} 
+               content={articles[currentIndex].content}
+               source={articles[currentIndex].source.name}
+               author={articles[currentIndex].author}/>
+          )
+    }
+    <button className="rightBtn text-white" onClick={rightHandle} > next </button>
     </div>
        
   )
 }
+             
