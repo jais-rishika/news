f103b2021e88f7263965e4a191f70486bc2de96b
Rishika Jaiswal <jaisrishika@gmail.com>
2024-12-06 19:45:47 +0530

    singleElement

--- src/App.js
+++ src/App.js
@@ -8,14 +8,22 @@ export default function App() {
   let touchEndX = 0;
 
   const handleTouchStart = (e) => {
-    touchStartX = e.targetTouches[0].clientX;
+    if (e.touches.length === 2) {
+      console.log("hello1")
+      touchStartX = e.touches[0].clientX; // Get the X position of the first finger
+    }
   };
 
   const handleTouchMove = (e) => {
-    touchEndX = e.targetTouches[0].clientX;
+    if (e.touches.length === 2){
+      console.log("hello2")
+      touchEndX = e.touches[0].clientX;
+    }
+
   };
 
   const handleTouchEnd = () => {
+
     if (touchStartX - touchEndX > 50) {
       // Swipe Left
       if (currentIndex < articles.length - 1) {
@@ -30,20 +38,28 @@ export default function App() {
       }
     }
   };
-
   return (
     <div 
-      className='bg-black items-center flex flex-col py-4 px-4 md:flex-row  h-screen md:overflow-x-auto scrollbar-hidden scrollbar-hidden-vertical'
-      
+      className='bg-black flex flex-row py-4 px-4 md:flex-row w-screen h-screen overflow-x-hidden px-50 '
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
     >
-        {articles.map((item,idx) => {
-          return (
-          <div className='inline-block min-w-max '>
-            <Card key={idx}  title={item.title} date={item.publishedAt.slice(0,10)} url={item.url} image={item.urlToImage} content={item.content}/>
-          </div>
-        )})}
-        {/* <Card/>
-        <Card/> */}
+        {/* {articles.map((item,idx) => {
+          return ( */}
+            <Card 
+              key={currentIndex} 
+              title={articles[currentIndex].title} 
+              date={articles[currentIndex].publishedAt.slice(0,10)} 
+              time={articles[currentIndex].publishedAt.slice(12,16)}
+              url={articles[currentIndex].url} 
+              image={articles[currentIndex].urlToImage} 
+              description={articles[currentIndex].description}
+              content={articles[currentIndex].content}
+              source={articles[currentIndex].source.name}
+              author={articles[currentIndex].author}
+            />
+        {/* )})} */}
     </div>
        
   )
--- src/Componennt/Card.jsx
+++ src/Componennt/Card.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 export default function Card({...data}) {
   return (
     // ${data.isCenter ? 'scale-110' : ''}
-    <div key={data.key} className={`flex flex-col items-center max-w-96 h-fit md:h-128 bg-gradient-to-b from-slate-300 to-slate-200 mx-14 mb-10 rounded-md p-4 shadow-lg shadow-gray-500 md:hover:scale-110`}>
+    <div key={data.key} className={`flex flex-col bg-gradient-to-b from-slate-300 to-slate-200 h-3/4 md:min-w-fit mr-10 px-5 rounded-md p-4 shadow-lg shadow-gray-500`}>
         <h1 className='text-2xl text-center font-bold'>{data.title}</h1>
-        <img src={data.image} className='rounded-md border-2 border-blue-50 mx-2 my-4 p-0 h-36 max-w-60 shadow-lg' alt={data.title}/>
-        <p className='rounded-md border-2 border-blue-50 p-1 px-2 line-clamp-4 '>{data.content}</p>
-        <button className='rounded-lg bg-blue-500 px-2 py-1 mt-2 transition-transform transform hover:scale-105'><a href={data.url }>Read More</a></button>
-        <div className='w-full flex justify-end'>
-          <span >{data.date}</span>
+        <div className='flex flex-row mt-11'>
+          <div>
+            <img src={data.image} className='rounded-md border-2 border-blue-50 mx-2 my-4 p-0 h-80 max-w-96 shadow-lg' alt={data.title}/>
+          </div>
+          <div className='flex flex-col justify-between'>
+            <p className='rounded-md border-2 border-blue-50 py-2 px-2 mt-4'>{data.description}</p>
+            <button className='rounded-lg bg-blue-500 px-2 py-1 mt-2 w-36 hover:scale-105'><a href={data.url}>Read More</a></button>
+            <div className='w-full flex flex-col items-end'>
+              <span>{data.source}</span>
+              <span>{data.author}</span>
+              <span >{data.date}</span>
+              <span >{data.time}</span>
+            </div>
+          </div>
         </div>
     </div>
   )
